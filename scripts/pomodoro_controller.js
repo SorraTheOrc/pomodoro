c4b1a859470e0167ea94cbfe491a2583b2a14108
Ross Gardler <rgardler@opendirective.com>
2013-01-08 20:17:31 +0000

    Change button text whenever clicked and add timer display when timer started

--- scripts/pomodoro_controller.js
+++ scripts/pomodoro_controller.js
@@ -1,7 +1,15 @@
 var ${widget.shortname}_timer_controller = { 
   init:function() { 
+      $('#remaining').hide();      
       $( "#action" ).click(function(event, ui) {
-          alert("hi there");
+          var text = $('#action .ui-btn-text').text();
+          if (text == "Stop") {
+              $('#action .ui-btn-text').text("Start");
+              alert("Not currently able to stop a running timer");
+          } else {
+              $('#action .ui-btn-text').text("Stop");
+              $('#remaining').show();
+          }
       });
   }
 };
